c3c9fd44e255604502784138350027532040beb2
annpia92 <anna.piatkowska19922gmail.com>
2018-06-09 17:16:28 +0200

    abc

---
+++ pesel/peseltools.py
@@ -0,0 +1,16 @@
+""""
+Checks if the given PESEL number is valid; if so, returns user's date of birth and gender
+"""
+
+import psl
+import validation
+
+if __name__ == "__main__":
+    try:
+        x = str(input('Podaj swoj numer PESEL: '))
+        if validation.validate(x) == False:
+            raise Exception('Wpisales nieprahwidlowy numer PESEL')
+        y = psl.extract_personal_data(x)
+        print("Data urodzenia: ", y["date_of_birth"], "Plec: ", y["sex"])
+    except Exception as error:
+        print(error)
---
+++ pesel/validation.py
@@ -0,0 +1,13 @@
+import correct_date
+def validate(x):
+    if correct_date.not_empty(x)==False:
+        return False
+    if correct_date.length_pesel(x)==False:
+        return False
+    if correct_date.is_positiveNumber(x) == False:
+        return False
+    if correct_date.val_checksum(x)==False:
+        return False
+    if correct_date.correctDate(x)==False:
+        return False
+    return True
